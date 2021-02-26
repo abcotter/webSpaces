c25912dfbd46e68ab3b89f0dff3402db1ff68e8f
Abigail Cotter <abigail.cotter@gmail.com>
2021-02-25 19:40:16 -0500

    code capture button to simply get current tabs and open new window with those tabs

--- popup.js
+++ popup.js
@@ -6,7 +6,7 @@
 // });
 
 window.onload = function () {
-	var spacesList = document.getElementById("main");
+	var spacesList = document.getElementById("spaces");
 
 	chrome.storage.sync.get("spaces", ({ spaces }) => {
 		if (spaces.length == 0) {
@@ -14,22 +14,27 @@ window.onload = function () {
 		}
 		spaces.forEach(space => spacesList.innerHTML += "<div> " + space + "</div>");
 	});
+
+	document.getElementById("capture").addEventListener("click", () => {
+		chrome.tabs.query({ currentWindow: true }, (tabs) => {
+			console.log(tabs)
+
+			tabIds = Array.from({ length: tabs.length }, (_, i) => i + tabs[0].id)
+			console.log(tabIds)
+
+			console.log(chrome.windows.WINDOW_ID_CURRENT)
+
+			chrome.windows.create({}, (window) => {
+				tabs.forEach(tab => {
+					console.log(tab)
+					chrome.tabs.create({
+						active: tab.active,
+						index: tab.index,
+						url: tab.url,
+						windowId: window.id
+					})
+				});
+			})
+		})
+	})
 };
-
-// When the button is clicked, inject setPageBackgroundColor into current page
-// changeColor.addEventListener("click", async () => {
-// 	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-// 	chrome.scripting.executeScript({
-// 		target: { tabId: tab.id },
-// 		function: setPageBackgroundColor,
-// 	});
-// });
-
-// // The body of this function will be executed as a content script inside the
-// // current page
-// function setPageBackgroundColor() {
-// 	chrome.storage.sync.get("color", ({ color }) => {
-// 		document.body.style.backgroundColor = color;
-// 	});
-// }
